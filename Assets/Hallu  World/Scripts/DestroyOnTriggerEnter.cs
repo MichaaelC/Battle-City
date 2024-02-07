29c911573f1fefcd8821636de979c88c9a1ce28f
Michael <116899709+MichaaelC@users.noreply.github.com>
2024-02-07 13:03:11 +0800

    Delete Assets/Hallu  World directory

--- Assets/Hallu  World/Scripts/DestroyOnTriggerEnter.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DestroyOnTriggerEnter : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Destroy(collision.gameObject);
-    }
-}
